c8224fc1859ada34e1bb952b373b6d1b26db7bf2
justabug190 <justin.adair@stonybrook.edu>
2016-10-10 00:25:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,8 @@ if [ -d "uno-zen" ]; then
   exit
 fi
 
-git clone https://github.com/Kikobeats/uno-zen.git && cd uno-zen
+git clone https://github.com/godofredoninja/Mapache.git && cd Mapache
 
-. "$PWD"/scripts/utils.sh
 
-welcome
-
-echo "Getting the repository tagged commits"
-git fetch --tags
-
-echo "Resolving the latest tagged version"
-latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-
-echo "Creating a stable branch from the latest tagged version"
-git checkout "$latestTag"
-git checkout -b stable
 
 echo "\nTheme Installed Successful! Enjoy :-)"
